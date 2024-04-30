a212d6bed72c7fb943cbcfbfee61cfdfb51a1672
Lelton Borges <leltondev@gmail.com>
2024-04-30 17:09:41 -0300

    add effect

--- src/app/components/core/menu/menu.component.ts
+++ src/app/components/core/menu/menu.component.ts
@@ -1,25 +1,53 @@
-import { Component, Input } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AnimationProp, FaIconComponent, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
-  selector: 'app-menu',
-  standalone: true,
-  imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatMenuModule,
-    MatTooltipModule,
-  ],
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.sass',
-})
+             selector: 'app-menu',
+             standalone: true,
+             imports: [
+               MatToolbarModule,
+               MatButtonModule,
+               MatIconModule,
+               MatSidenavModule,
+               MatMenuModule,
+               MatTooltipModule,
+               FontAwesomeModule
+             ],
+             templateUrl: './menu.component.html',
+             styleUrl: './menu.component.sass'
+           })
 export class MenuComponent {
-  
+  private _matButton!: MatButton;
+  private _selectIcon!: FaIconComponent;
+
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
+
+  setAnimationIcon(matbutton: MatButton,
+                   icon: FaIconComponent,
+                   animation: string){
+    if(this._matButton)
+      this._matButton._elementRef.nativeElement.classList.remove("active")
+
+    if(this._selectIcon) {
+      this._selectIcon.animation = undefined;
+      this._selectIcon.render();
+    }
+
+    matbutton._elementRef.nativeElement.classList.add("active")
+    this._matButton = matbutton
+
+    icon.animation = animation as AnimationProp;
+    icon.render()
+    this._selectIcon = icon;
+  }
 }
