1a98a008c1fea480d04c1a47daed497f6e2c2dba
Lelton Borges <leltondev@gmail.com>
2024-05-31 19:52:26 -0300

    feat: set rules neighbor move

--- front/src/app/components/minefield/play/play.component.ts
+++ front/src/app/components/minefield/play/play.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CellComponent } from '../cell/cell.component';
+import { NotificationService } from '../../../service/role/notification.service';
+import { MoveService } from '../../../service/role/move.service';
 
 @Component({
-  selector: 'app-play',
-  standalone: true,
+             selector: 'app-play',
+             standalone: true,
              imports: [
                CellComponent
              ],
-  templateUrl: './play.component.html',
-  styleUrl: './play.component.sass'
-})
+             templateUrl: './play.component.html',
+             styleUrl: './play.component.sass'
+           })
 export class PlayComponent {
   private _cellComponent!: CellComponent;
   @Input() cols: number = 5;
@@ -17,31 +19,40 @@ export class PlayComponent {
   rowsArray: number[] = [];
   colsArray: number[] = [];
 
-  constructor() {}
+  constructor(private _notifyService: NotificationService,
+              private _moveService: MoveService) {}
 
   cellClicked(cell: CellComponent) {
+    if (this.isEqualsCell(cell)) {
+      this._notifyService.notify('Sua posição já é esta!');
+      return;
+    }
+
     if (this._cellComponent) {
       this._cellComponent.noAction();
     }
-    cell.toReveal();
-    this._cellComponent = cell;
+
+    if (this.canTransact(cell)) {
+      cell.toReveal();
+      this._cellComponent = cell;
+      this._notifyService.notify('Transação realizada!');
+    } else this._notifyService.notify('Vizinhação invalida!');
+  }
+
+  private isEqualsCell(cell: CellComponent): boolean {
+    return this._moveService.isEqualsCell(this._cellComponent, cell);
+  }
+
+  private canTransact(cell: CellComponent): boolean {
+    if (this._cellComponent) {
+      const col: number = this._cellComponent.col;
+      const row: number = this._cellComponent.row;
+      return this._moveService.canMove(cell, col, row);
+    } else return true;
   }
 
   ngOnInit(): void {
     this.rowsArray = Array.from({ length: this.rows }, (_, i) => i);
     this.colsArray = Array.from({ length: this.cols }, (_, i) => i);
-  }
-
-  isDragging: boolean = false;
-
-  onMouseDown(event: MouseEvent, positon: {row: number, col: number}) {
-    this.isDragging = false
-    console.log("Down");
-  }
-
-
-  onMouseUp(event: MouseEvent, positon: {row: number, col: number}) {
-    this.isDragging = true;
-    console.log("UP");
   }
 }
---
+++ front/src/app/service/role/move.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { CellComponent } from '../../components/minefield/cell/cell.component';
+import { Neighbor, NEIGHBORS } from '../../core/interface/neighbor';
+
+@Injectable({
+              providedIn: 'root'
+            })
+export class MoveService {
+
+  constructor() { }
+
+  isEqualsCell(current: CellComponent,
+               cell: CellComponent): boolean {
+    if (current && cell) {
+      return current.col === cell.col
+             && current.row === cell.row;
+    } else return false;
+  }
+
+  canMove(cell: CellComponent,
+          col: number,
+          row: number) {
+    return NEIGHBORS.filter(this.checkCellMatch(cell, col, row)).length > 0;
+  }
+
+  private checkCellMatch(cell: CellComponent,
+                         col: number,
+                         row: number) {
+    return (n: Neighbor) => this.isColumnMatch(cell, n, col)
+                            && this.isRowMatch(cell, n, row);
+  }
+
+  private isRowMatch(cell: CellComponent,
+                     n: Neighbor,
+                     row: number) {
+    return cell.row + n.row === row;
+  }
+
+  private isColumnMatch(cell: CellComponent,
+                        n: Neighbor,
+                        col: number) {
+    return cell.col + n.column === col;
+  }
+
+}
